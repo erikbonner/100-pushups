7520d6f1bd6ec5179d3a2d140964cd6846056033
NerdFlanders <jenja.dietrich@redheads.de>
2019-01-17 18:32:45 +0100

    added timer and week 6

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatTableModule} from '@angular/material/table';
 import {MatExpansionModule} from '@angular/material/expansion';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatButtonModule} from '@angular/material/button';
+
 @NgModule({
   exports: [
     MatTabsModule,
     MatTableModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
   ],
 })
 export class AppMaterialModule { }
